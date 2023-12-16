a4936438049f5b129e89c553b2ee279a6fca9b8e
Michelle Ndiangui <ndianguimichelle@gmail.com>
2023-12-16 19:37:52 +0300

    setting up for authentication

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+export default function Navbar(){
+    return (
+        <nav>
+            <Image
+                src="/menuase.png"
+                alt="Menuase App"
+                width={200}
+                height={75}
+                priority
+            />
+        </nav>
+    );
+}

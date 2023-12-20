4cffb943cc7acbb3249a30c1a2746d409f638478
Michelle Ndiangui <ndianguimichelle@gmail.com>
2023-12-21 00:00:25 +0300

    status: bug fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,10 +6,6 @@ import Navbar from './components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] })
 
-// export const metadata = {
-//   title: 'Menuase App',
-//   description: 'Your meal prepping partner. Get access to thousands of recipes and generate meal plans.',
-// }
 
 export default function RootLayout({ children }) {
   return (
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 "use client"
-import Navbar from './components/Navbar';
 import { SessionProvider } from 'next-auth/react';
 
 export default function Home() {
   return (
     <SessionProvider>
       <main className="w-full min-h-screen flex-col items-center justify-between p-5">
+        <h1>Homepage</h1>
       </main>
     </SessionProvider>
   )

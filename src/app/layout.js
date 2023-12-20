5111f4eb6d0c5b8abf54d2dd5f5bbb4a6bce2bc4
Michelle Ndiangui <ndianguimichelle@gmail.com>
2023-12-20 22:41:27 +0300

    removing export: bug fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,10 +5,10 @@ import { AuthContextProvider } from './context/AuthContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Menuase App',
-  description: 'Your meal prepping partner. Get access to thousands of recipes and generate meal plans.',
-}
+// export const metadata = {
+//   title: 'Menuase App',
+//   description: 'Your meal prepping partner. Get access to thousands of recipes and generate meal plans.',
+// }
 
 export default function RootLayout({ children }) {
   return (
